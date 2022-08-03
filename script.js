7bd69b3082f1a092c53ccc59be9b33b0de44bfbd
kate115 <75540720+kate115@users.noreply.github.com>
2022-08-03 07:21:33 -0700

    js test stuff

---
+++ script.js
@@ -0,0 +1,15 @@
+// Rabbit Pet Data
+var rabbitName = "Petra";
+var rabbitAge = 3;
+var isCute = true;
+var rating = 4.5
+
+const myRabbit = {
+    firstName: "Petra",
+    lastName: "Wang",
+    dateOfBirth: 8/4/21,
+    age: 2022 - dateOfBirth,
+    dateOfDeath: 1/1/22,
+    hobbies: ['playing', 'eating'],
+    ownerName: "Melody Wang"
+}
